afb29054f6397df8eda5c531bfe7d60bbeb00246
Ethan Eldridge <ejayeldridge@gmail.com>
2018-02-11 17:36:40 -0500

    Update AbstractFile with id fields for subclasses
    Can't have the depend on hashcode since the id would change when we're
    adding in file tags and other such junk. Why didn't I think of that
    before?

--- app/model/domain/AbstractFile.scala
+++ app/model/domain/AbstractFile.scala
@@ -6,13 +6,14 @@ import java.time.Instant
 sealed abstract trait AbstractFile {
 	/** @note Is the hashcode of the File itself
 	 */
-	def id: Int = path.hashCode
+	def id: Int
 	def path: Path
 	def maybeIndexedAt: Option[Instant]
 	def isTagged: Boolean
 }
 
 case class TaggedFile(
+		id: Int,
 		path: Path,
 		maybeIndexedAt: Option[Instant],
 		tags: Vector[FileTag],
@@ -22,8 +23,15 @@ case class TaggedFile(
 }
 
 case class QueuedFile(
+		id: Int,
 		path: Path,
 		maybeIndexedAt: Option[Instant]
 ) extends AbstractFile {
 	def isTagged = false
+}
+
+object QueuedFile {
+	def apply(path: Path): QueuedFile = {
+		QueuedFile(path.hashCode, path, None)
+	}
 }
